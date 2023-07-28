c169f01b2a92cd88a14e543cb752d129db850981
fuyaozhao1018 <fuyaozhao1018@gmail.com>
2023-07-28 04:10:00 -0700

    update css

--- vue-project/tailwind.config.js
+++ vue-project/tailwind.config.js
@@ -8,6 +8,17 @@ module.exports = {
         "weather-secondary": "#004E71",
       },
     },
+    fontFamily: {
+      Roboto: ["Roboto, sans-serif"],
+    },
+    container: {
+      padding: "2em",
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+    },
   },
   plugins: [],
 }
