172b28065344f2f3360a0da9836ddccb2159e576
fuyaozhao1018 <fuyaozhao1018@gmail.com>
2023-07-30 00:38:14 -0700

    update to main

---
+++ vue-project/src/components/CityViewSkeleton.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="flex flex-col flex-1">
+      <!-- Overview -->
+      <div class="flex flex-col py-12 items-center">
+        <AnimationSkeleton class="max-w-[300px] w-full mb-2" />
+        <AnimationSkeleton class="max-w-[300px] w-full mb-12" />
+        <AnimationSkeleton
+          class="max-w-[300px] h-[100px] w-full mb-12"
+        />
+        <AnimationSkeleton class="max-w-[300px] w-full mb-8" />
+        <AnimationSkeleton
+          class="max-w-[300px] h-[75px] w-full"
+        />
+      </div>
+      <!-- Hourly -->
+      <div class="flex flex-col py-12 px-8 items-center">
+        <AnimationSkeleton class="max-w-screen-md 
+                                  h-[100px] w-full mb-12 "/>
+      </div>
+      <!-- Weekly -->
+      <div class="flex flex-col py-12 px-8 items-center">
+        <AnimationSkeleton class="max-w-screen-md 
+                                  h-[100px] w-full mb-12"/>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import AnimationSkeleton from "./AnimationSkeleton.vue";
+  </script>
